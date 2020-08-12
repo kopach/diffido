54459565d8d3a0d08cb6e5ed879a94fe7a153a66
Ihor Kopach <kopach@users.noreply.github.com>
2020-08-13 00:11:02 +0200

    Make docke auto restart unless stopped

--- run-docker.sh
+++ run-docker.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 docker build -t davidealberani/diffido .
-docker run --rm -it -v `pwd`/conf:/diffido/conf -v `pwd`/storage:/diffido/storage -p 3210:3210 davidealberani/diffido --debug
-
+docker run --restart unless-stopped -d -it -v $(pwd)/conf:/diffido/conf -v $(pwd)/storage:/diffido/storage -p 3210:3210 davidealberani/diffido --debug
